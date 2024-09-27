62ec589170db2cbbbf5bc56852e7ae169b0637fd
asheesh kushwah <asheesh94065@gmail.com>
2024-09-27 18:39:02 +0530

    reawsfgdfv

---
+++ loop/pass.py
@@ -0,0 +1,19 @@
+# student =["ram",23,"delhi"]
+
+# for a in student:
+#     pass
+# for b in student:
+#     print(b)
+
+
+
+a=100
+b=300
+
+if(a==b):
+        print("yes")
+elif(a<b):
+    pass
+    print(a)
+else:
+    print("kuch bhi nahi")
---
+++ loop/tablepro.py
@@ -0,0 +1,13 @@
+num =int(input("enter the number:"))
+for i in range (1,11):
+    print(f"{num}x{i}={num*i}")   #f-string
+
+
+
+
+
+# b=10
+# i=1
+# while i<=b:
+#     print(table,"x",i,"=",table*i)
+#     i=i+1
