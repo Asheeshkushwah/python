7c7938e64447267b02356a3c622711057a8893bf
asheesh kushwah <asheesh94065@gmail.com>
2024-11-22 17:54:11 +0530

    ytrfhtgn

---
+++ programs/atomatic vending.py
@@ -0,0 +1,58 @@
+a=input("enter code of item or frist letter of item:")
+b=int(input("enter the index number:"))
+
+if(a=='c'and b==1):
+    print("welcome to ccd")
+    print("espresso coffee")
+elif(a=='c' and b==2):
+    print("welcome to ccd")
+    print("cappuccino coffee") 
+elif(a=="c" and b==3):
+    print("welcome to ccd") 
+    print("latte cofffee")
+elif(a=='t' and b==1):
+    print("welcome to ccd") 
+    print("enjoy to plain tea")
+elif(a=='t' and b==2):
+    print("welcome to ccd") 
+    print("enjoy to asam tea")  
+elif(a=='t' and b==3):
+    print("welcome to ccd") 
+    print("enjoy to ginger tea")  
+elif(a=='t' and b==4):
+    print("welcome to ccd") 
+    print("enjoy to cardamon tea") 
+elif(a=='t' and b==5):
+    print("welcome to ccd") 
+    print("enjoy to masala tea") 
+elif(a=='t' and b==6):
+    print("welcome to ccd") 
+    print("enjoy to lemon tea") 
+elif(a=='t' and b==7):
+    print("welcome to ccd") 
+    print("enjoy to green tea") 
+elif(a=='t' and b==8):
+    print("welcome to ccd") 
+    print("enjoy to special tea") 
+elif(a=='s' and b==1):
+    print("welcome to ccd") 
+    print("enjoy to hot and sour soup")  
+elif(a=='s' and b==2):
+    print("welcome to ccd") 
+    print("enjoy to veg corn soup")
+elif(a=='s' and b==3):
+    print("welcome to ccd") 
+    print("enjoy to tomato soup")
+elif(a=='s' and b==4):
+    print("welcome to ccd") 
+    print("enjoy to spicy tomato soup") 
+elif(a=='b' and b==1):
+    print("welcome to ccd") 
+    print("enjoy to hot chocolate drink") 
+elif(a=='b' and b==2):
+    print("welcome to ccd") 
+    print("enjoy to badam drink") 
+elif(a=='b' and b==3):
+    print("welcome to ccd") 
+    print("enjoy to badam-pista drink") 
+else:("invalid input")            
